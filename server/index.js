6ed07ab621a4ee95ccfbf205150ffde1d34c852c
StyeEye <ColonialBuff@protonmail.com>
2019-05-16 16:11:38 -0600

    Database setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const controller = require("./controller");
+const massive = require("massive");
+require("dotenv").config();
 
 const server = express();
+
+massive(process.env.CONNECTION_STRING)
+    .then(database => {
+        server.set("db", database);
+        console.log("Database initialized");
+    })
+    .catch(error => {
+        console.log(`Database initialization failed. Reason: ${error}`);
+    });
 
 const port = process.env.PORT || 8080;
 
